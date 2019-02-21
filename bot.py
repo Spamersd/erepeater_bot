73c7f7e918be496efdb1d0d21702a0cfd5fe2c45
Alexandr Drykov <spamersd@gmail.com>
2019-02-21 11:08:11 +0700

    Add new parser and shrink long message

--- bot.py
+++ bot.py
@@ -4,13 +4,17 @@ import telebot
 import imaplib
 import email
 import time
+import logging
 
 from time import sleep
 from telebot import apihelper
 from bs4 import BeautifulSoup
 
-
 apihelper.proxy = {'https':'socks5://localhost:9050'}
+
+FORMAT = "%(asctime)s;%(levelname)s;%(message)s"
+
+logging.basicConfig(filename="bot.log", level=logging.INFO, format=FORMAT)
 
 bot = telebot.TeleBot(config.token)
 
@@ -43,29 +47,45 @@ def parse_message(data):
     msg_from = getHeader(msg,'From')
     msg_subject = getHeader(msg,'Subject')
     msg_body = getBody(msg)
-    msg = msg_date + '\n' + msg_from +'\n' + msg_subject +'\n'+ msg_body 
+    msg = cut_message(msg_date + '\n' + msg_from +'\n' + msg_subject +'\n'+ msg_body)
+    
+    return msg
+    pass
+
+def cut_message (msg):
+    while "  " in msg:
+        msg= msg.replace("  ", " ")
+
+    msg = msg.expandtabs(1)
+    msg = msg.strip()
+    if len(msg) > 500:
+        msg = msg[0:500]
 
     return msg
     pass
 
 def getBody(msg):
     """ Extract content body of an email messsage """
-    body = ""
-    if msg.is_multipart():
-        for payload in msg.get_payload():
-            charset = payload._charset
+    try:
+        if msg.is_multipart():
+            body = ""
+            for payload in msg.get_payload():
+                body = body + getBody(payload)
+
+        else:
+            charset = msg._charset
             if charset is None:
                 charset = 'utf-8' 
-            # if payload.is_multipart(): ...
-            body = body + payload.get_payload(decode=True).decode(charset)
-    else:
-        charset = msg._charset
-        if charset is None:
-            charset = 'utf-8' 
-        body = msg.get_payload(decode=True).decode(charset)
-
-    soup = BeautifulSoup(body)
-    return soup.get_text()
+            body = msg.get_payload(decode=True).decode(charset)
+            soup = BeautifulSoup(body)
+            return soup.get_text()  
+        pass
+    except Exception as e:
+        body = "Ошибка обработки тела письма" + str(e)
+        logging.error(body)
+        pass
+    
+    return body
 
 def getHeader(Message,Attribute):
     
@@ -91,7 +111,7 @@ def getHeader(Message,Attribute):
             pass
 
         pass
-    except:
+    except Exception as e:
         pass   
     
     return text
@@ -104,8 +124,8 @@ def sendbot(msg):
     try:
         bot.send_message(config.CHANNEL_NAME, msg)
         pass
-    except:
-        print("Ошибка отправки собщения ботом")
+    except Exception as e:
+        logging.error("Error send message from bot" + str(e))
         result = False
         pass
     return result
@@ -113,11 +133,14 @@ def sendbot(msg):
 
 if __name__ == '__main__':
 
-     while True:
+    logging.info("Bot was started")
+
+    while True:
         try:
             check_email()
-        except:
-            sendbot("Ошибка проверки почты!")
+        except Exception as e:
+            sendbot("Ошибка проверки почты!\n"+"Ошибка: "+str(e))
+            logging.error("Error check email: "+ str(e))
         time.sleep(300)
         pass
 
